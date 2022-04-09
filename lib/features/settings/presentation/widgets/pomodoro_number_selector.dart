8ecca60f381a8311b1f548138dc1093415a6d1b8
Ryan Yip <71072280+LuckUVeryX@users.noreply.github.com>
2022-04-09 13:15:09 +0800

    Feat/theming (#5)
    * adds dracula theme
    
    * watches theme
    
    * minor ui tweaks

--- lib/features/settings/presentation/widgets/pomodoro_number_selector.dart
+++ lib/features/settings/presentation/widgets/pomodoro_number_selector.dart
@@ -15,52 +15,56 @@ class PomodoroNumberSelector extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final textTheme = theme.textTheme;
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: ExpansionTile(
-        collapsedBackgroundColor: theme.cardColor,
-        backgroundColor: theme.cardColor,
-        childrenPadding: const EdgeInsets.only(bottom: 24.0),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              'Pomodoros',
-              style:
-                  textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold),
-            ),
-            Text(
-              '4',
-              style: textTheme.titleMedium?.copyWith(
-                color: theme.primaryColor,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+      clipBehavior: Clip.antiAlias,
+      child: Theme(
+        data: theme.copyWith(
+          dividerColor: Colors.transparent,
         ),
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+        child: ExpansionTile(
+          childrenPadding: const EdgeInsets.only(bottom: 24.0),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              _IncrementDecrementButton(
-                onPressed: decrementOnPressed,
-                iconData: FontAwesomeIcons.minus,
+              Text(
+                'Pomodoros',
+                style: textTheme.titleMedium
+                    ?.copyWith(fontWeight: FontWeight.bold),
               ),
-              const SizedBox(width: 24.0),
               Text(
                 '4',
-                style: textTheme.headlineSmall?.copyWith(
+                style: textTheme.titleMedium?.copyWith(
+                  color: theme.colorScheme.primary,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              const SizedBox(width: 24.0),
-              _IncrementDecrementButton(
-                onPressed: incrementOnPressed,
-                iconData: FontAwesomeIcons.plus,
-              ),
             ],
-          )
-        ],
+          ),
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _IncrementDecrementButton(
+                  onPressed: decrementOnPressed,
+                  iconData: FontAwesomeIcons.minus,
+                ),
+                const SizedBox(width: 24.0),
+                Text(
+                  '4',
+                  style: textTheme.headlineSmall?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(width: 24.0),
+                _IncrementDecrementButton(
+                  onPressed: incrementOnPressed,
+                  iconData: FontAwesomeIcons.plus,
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: unused_field
+
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  final _draculaColorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: _DraculaColors.purple,
+    primary: _DraculaColors.purple,
+    background: _DraculaColors.background,
+    onBackground: _DraculaColors.foreground,
+    error: _DraculaColors.red,
+  );
+
+  ThemeData get draculaTheme {
+    return ThemeData.from(
+      colorScheme: _draculaColorScheme,
+    ).copyWith(
+      appBarTheme: AppBarTheme(
+        backgroundColor: _draculaColorScheme.background,
+        elevation: 0,
+      ),
+    );
+  }
+}
+
+class _DraculaColors {
+  static const background = Color(0xff282a36);
+  static const currentLine = Color(0xff44475a);
+  static const foreground = Color(0xfff8f8f2);
+  static const comment = Color(0xff6272a4);
+  static const cyan = Color(0xff8be9fd);
+  static const green = Color(0xff50fa7b);
+  static const orange = Color(0xffffb86c);
+  static const pink = Color(0xffff79c6);
+  static const purple = Color(0xffbd93f9);
+  static const red = Color(0xffff5555);
+  static const yellow = Color(0xfff1fa8c);
+}
