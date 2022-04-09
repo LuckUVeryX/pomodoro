8d13dd339e65d62a6961f3612069499ff8a0fbbf
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2022-04-09 10:52:40 +0800

    adds logging

---
+++ lib/core/utils/logging.dart
@@ -0,0 +1,36 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:logger/logger.dart';
+
+// * Logger
+final loggerProvider = Provider<Logger>((ref) => AppLogger());
+
+class AppLogger extends Logger {
+  AppLogger()
+      : super(
+          filter: ProductionFilter(),
+          printer: PrettyPrinter(
+            noBoxingByDefault: true,
+            methodCount: 0,
+          ),
+        );
+}
+
+class ProviderLogObserver extends ProviderObserver {
+  ProviderLogObserver(this._log);
+
+  final Logger _log;
+
+  @override
+  void didUpdateProvider(
+    ProviderBase provider,
+    Object? previousValue,
+    Object? newValue,
+    ProviderContainer container,
+  ) {
+    _log.d(
+      '''
+  "provider": "${provider.name ?? provider.runtimeType}",
+  "newValue": "$newValue"''',
+    );
+  }
+}
